f1e63f5d25b571537540847749aa984e5d1afa67
NightWolf328 <deondreivory328@gmail.com>
2025-02-19 07:26:16 -0500

    Finish task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,14 +32,17 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title;
+  let filtered_titles = Object.values(books).filter(book => book.title === title);
+  if(filtered_titles.length > 0)
+    return res.status(200).json(filtered_titles);
+  return res.status(404).json({message : "not found"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let book = books[req.params.isbn];
+    return res.status(200).json({"Review" : book.reviews});
 });
 
 module.exports.general = public_users;
